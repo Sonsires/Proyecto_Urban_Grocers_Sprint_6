e6baebd94cea0a214e02059b714cd56436deca02
Sonsiré Sánchez <145903696+Sonsires@users.noreply.github.com>
2023-10-09 16:40:46 +0100

    Nuevas correcciones

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -1,56 +1,54 @@
 import data
-import sender_stand_request
+import sender_stand_request as req
 
-def validacion_positiva(name):
-    kit_response = sender_stand_request.post_new_client_kit(data.kit_body[name])
+def validacion_positiva(kit_body):
+    kit_response = req.crear_kit(kit_body)
     assert kit_response.status_code == 201
-    assert kit_response.json()["name"] != ""
-    assert kit_response.json()["name"] == data.kit_body[name]["name"]
-    print("Resultado de la creación del kit:", kit_response.json())
+    assert kit_response.json()["name"] == kit_body["name"]
+
 
 def validacion_negativa(name):
-    kit_response = sender_stand_request.post_new_client_kit(data.kit_body[name])
+    kit_response = req.crear_kit(name)
     assert kit_response.status_code == 400
     assert kit_response.json()["name"] != ""
-    print(kit_response.status_code)
+
 
 
 
 def test_create_kit_name_1_letter_result_201():
-    validacion_positiva('prueba1')
-    print(test_create_kit_name_1_letter_result_201)
+    validacion_positiva(data.kit_body['prueba1'])
+
 
 def test_create_kit_name_511_letter_result_201():
-    validacion_positiva('prueba2')
-    print(test_create_kit_name_511_letter_result_201)
+    validacion_positiva(data.kit_body['prueba2'])
+
 
 def test_create_kit_name_caracther_letter_result_201():
-    validacion_positiva('prueba5')
-    print(test_create_kit_name_caracther_letter_result_201)
+    validacion_positiva(data.kit_body['prueba5'])
+
 
 def test_create_kit_name_space_letter_result_201():
-    validacion_positiva('prueba6')
-    print(test_create_kit_name_space_letter_result_201)
+    validacion_positiva(data.kit_body['prueba6'])
+
 
 def test_create_kit_name_number_letter_result_201():
-    validacion_positiva('prueba7')
-    print(test_create_kit_name_number_letter_result_201)
+    validacion_positiva(data.kit_body['prueba7'])
+
 
 
 def test_create_kit_name_vacio_letter_result_400():
-    validacion_negativa('prueba3')
-    print(test_create_kit_name_vacio_letter_result_400)
+    validacion_negativa(data.kit_body['prueba3'])
+
 
 def test_create_kit_name_512_letter_result_400():
-    validacion_negativa('prueba4')
-    print(test_create_kit_name_512_letter_result_400)
+    validacion_negativa(data.kit_body['prueba4'])
+
 
 def test_create_kit_name_no_se_pasa_la_solicitud_letter_result_400():
-    validacion_negativa('prueba8')
-    print(test_create_kit_name_no_se_pasa_la_solicitud_letter_result_400)
+    validacion_negativa(data.kit_body['prueba8'])
+
 
 
 def test_create_kit_name_parametro_diferente_letter_result_400():
-    validacion_negativa('prueba9')
-    print(test_create_kit_name_parametro_diferente_letter_result_400)
+    validacion_negativa(data.kit_body['prueba9'])
 
